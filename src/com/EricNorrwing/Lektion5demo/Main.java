d893621895919ecb35b6b54783960dd0610be0ac
EricNorrwing <Eric.Norrwing@gmail.com>
2023-10-26 10:11:40 +0200

    delay

--- src/com/EricNorrwing/Lektion5demo/Main.java
+++ src/com/EricNorrwing/Lektion5demo/Main.java
@@ -1,9 +1,21 @@
 package com.EricNorrwing.Lektion5demo;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-
+        // Delay output for game to make it more readable?
+        System.out.print("H");
+        Thread.sleep(300);
+        System.out.print("E");
+        Thread.sleep(300);
+        System.out.print("L");
+        Thread.sleep(300);
+        System.out.print("L");
+        Thread.sleep(300);
+        System.out.print("O");
+        Thread.sleep(300);
 
 
 
