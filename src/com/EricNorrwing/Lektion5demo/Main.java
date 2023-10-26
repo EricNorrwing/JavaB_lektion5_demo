09f8d9db2c3d8bb4d03d1587c233c730f291d345
EricNorrwing <Eric.Norrwing@gmail.com>
2023-10-26 12:03:57 +0200

    stuff

--- src/com/EricNorrwing/Lektion5demo/Main.java
+++ src/com/EricNorrwing/Lektion5demo/Main.java
@@ -1,29 +1,28 @@
 package com.EricNorrwing.Lektion5demo;
 
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        //TODO There is a color class in slide lektion 5
         // Delay output for game to make it more readable?
         System.out.print("H");
-        Thread.sleep(300);
+        Thread.sleep(100);
         System.out.print("E");
-        Thread.sleep(300);
+        Thread.sleep(100);
         System.out.print("L");
-        Thread.sleep(300);
+        Thread.sleep(100);
         System.out.print("L");
-        Thread.sleep(300);
-        System.out.print("O");
-        Thread.sleep(300);
+        Thread.sleep(100);
+        System.out.println("O");
 
-
-
-
-
+        helloWorld();
+        
     }
 
-
+public static void helloWorld(){
+    System.out.println("Hello world");
+}
 
 
 
